07f70604b844763526f91b7b144e970dcea5d0db
azam <azamshul@gmail.com>
2025-05-15 05:33:38 +0900

    Add more benchmarks

--- src/test/java/io/azam/ulidj/MonotonicULIDBenchmark.java
+++ src/test/java/io/azam/ulidj/MonotonicULIDBenchmark.java
@@ -7,19 +7,39 @@ import org.openjdk.jmh.annotations.Setup;
 import org.openjdk.jmh.annotations.State;
 import org.openjdk.jmh.infra.Blackhole;
 
+import java.security.SecureRandom;
+import java.util.Random;
+
 public class MonotonicULIDBenchmark {
   @State(Scope.Thread)
   public static class MonotonicULIDState {
-    public MonotonicULID instance;
+    public MonotonicULID random;
+    public MonotonicULID secureRandom;
 
     @Setup(Level.Trial)
     public void doSetup() {
-      instance = new MonotonicULID();
+      random = new MonotonicULID(new Random());
+      secureRandom = new MonotonicULID(new SecureRandom());
     }
   }
 
   @Benchmark
-  public void generate(Blackhole blackhole, MonotonicULIDState state) {
-    blackhole.consume(state.instance.generate());
+  public void generateRandom(Blackhole blackhole, MonotonicULIDState state) {
+    blackhole.consume(state.random.generate());
+  }
+
+  @Benchmark
+  public void generateBinaryRandom(Blackhole blackhole, MonotonicULIDState state) {
+    blackhole.consume(state.random.generateBinary());
+  }
+
+  @Benchmark
+  public void generateSecureRandom(Blackhole blackhole, MonotonicULIDState state) {
+    blackhole.consume(state.secureRandom.generate());
+  }
+
+  @Benchmark
+  public void generateBinarySecureRandom(Blackhole blackhole, MonotonicULIDState state) {
+    blackhole.consume(state.secureRandom.generateBinary());
   }
 }
--- src/test/java/io/azam/ulidj/ULIDBenchmark.java
+++ src/test/java/io/azam/ulidj/ULIDBenchmark.java
@@ -8,14 +8,30 @@ import org.openjdk.jmh.infra.Blackhole;
 
 public class ULIDBenchmark {
   private static final String TEST_VALUE = "00A9G895B0ZZZZZZZZZZZZZZZZ";
+  private static final byte[] TEST_BINARY = new byte[] { //
+      (byte) 0x00, (byte) 0x52, (byte) 0x60, (byte) 0x84, //
+      (byte) 0x95, (byte) 0x60, (byte) 0xff, (byte) 0xff, //
+      (byte) 0xff, (byte) 0xff, (byte) 0xff, (byte) 0xff, //
+      (byte) 0xff, (byte) 0xff, (byte) 0xff, (byte) 0xff //
+  };
 
   @Benchmark
-  public void random(Blackhole blackhole) {
+  public void generateRandom(Blackhole blackhole) {
     blackhole.consume(ULID.random(new Random()));
   }
 
   @Benchmark
-  public void secureRandom(Blackhole blackhole) {
+  public void generateBinaryRandom(Blackhole blackhole) {
+    blackhole.consume(ULID.randomBinary(new Random()));
+  }
+
+  @Benchmark
+  public void generateSecureRandom(Blackhole blackhole) {
+    blackhole.consume(ULID.random(new SecureRandom()));
+  }
+
+  @Benchmark
+  public void generateBinarySecureRandom(Blackhole blackhole) {
     blackhole.consume(ULID.random(new SecureRandom()));
   }
 
@@ -27,5 +43,15 @@ public class ULIDBenchmark {
   @Benchmark
   public void getEntropy(Blackhole blackhole) {
     blackhole.consume(ULID.getEntropy(TEST_VALUE));
+  }
+
+  @Benchmark
+  public void toBinary(Blackhole blackhole) {
+    blackhole.consume(ULID.toBinary(TEST_VALUE));
+  }
+
+  @Benchmark
+  public void fromBinary(Blackhole blackhole) {
+    blackhole.consume(ULID.fromBinary(TEST_BINARY));
   }
 }
