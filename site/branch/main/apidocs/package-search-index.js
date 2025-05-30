9ed1cb1a20a9d39e9aa8c66cd588a97590162017
azam <azam@users.noreply.github.com>
2025-05-30 21:16:39 +0000

    deploy: 0ef67e21bbeea5c4516c6346b236a2ed3d0be4b6

---
+++ site/branch/main/apidocs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"m":"ulidj","l":"io.azam.ulidj"}]
---
+++ site/branch/main/testapidocs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"io.azam.ulidj"},{"l":"io.azam.ulidj.jmh_generated"}]
